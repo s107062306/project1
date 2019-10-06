4ddaf5aa711a1665a32f5d6c20ee3df0bea1ca41
wushangtse <wu.shang.tse@gmail.com>
2019-10-06 13:54:16 +0800

    0.0

--- main.cpp
+++ main.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+int row, col = 0;/*the size of the game matrix*/
+
+class block{
+    
+};
+
+int main(void){
+    cin >>row >>col ;
+    int
+    
+}
