a9862e48fe614ec0c121c9e4f3b62fab29b88ca9
wushangtse <wu.shang.tse@gmail.com>
2019-10-10 12:15:04 +0800

    0.1.2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 
 //name different blocks
+int row, col = 0;/*the size of the game matrix*/
+int term = 1; /*to decide whether the game is over or not*/
 int T1[4][4]{
     {0,0,0,0},
     {0,0,0,0},
@@ -100,19 +102,93 @@ int Z2[4][4]{
     {1,1,0,0},
     {1,0,0,0}
 };
+int I1[4][4]{
+    {1,0,0,0},
+    {1,0,0,0},
+    {1,0,0,0},
+    {1,0,0,0}
+};
+int I2[4][4]{
+    {0,0,0,0},
+    {0,0,0,0},
+    {0,0,0,0},
+    {1,1,1,1}
+};
+int O[4][4]{
+    {0,0,0,0},
+    {0,0,0,0},
+    {1,1,0,0},
+    {1,1,0,0}
+};
 
-int row, col = 0;/*the size of the game matrix*/
+void drop(int A[4][4], int loc, int map[10][20]){
+    int place;/*the exact row it would land on*/
+    for(int i = 0; i < row;)
+    {
+        int fin = 1;
+        for(int j = 0; j < 4 ; j++)
+        {
+            for(int k = 0; k < 4 ; k++)
+            {
+                if(A[j][k] + map[i+j][loc+k] == 2)
+                {
+                    fin = 0;
+                    i++;
+                    continue;
+                } 
+            }
+            if(fin == 0)continue;           
+        }
+        if(fin == 0)continue;
+    }/*decide location*/
+
+    for(int i = 0 ; i < col; i++){
+        if(map[row][i] == 1){
+            term = 0;
+            break;
+        }
+        else continue;
+    }/*check game over*/
+
+};
+
 
 int main(void){
     ifstream fin("tetris.data");
     ofstream fout("tetris.final");
-    fin >>row >>col ;
+    fin >>row >>col;
     int map[row+4][col];
     for(int i = 0; i < row+4; i++){
         for(int j = 0; j < col; j++){
             map[i][j] = 0;
         }
     }
+    char cmd[3];
+    fin >> cmd;
+    int loc;//the location of the block
+    while(cmd[0]!='E'){
+        fin >> loc;
+        loc = loc - 1;
+        int stop = 0;
+        if(cmd[0] == 'T'){
+            if(cmd[1] == '1'){
+                for(int i = row;i >= 0;i--){
+                    if(map[i][loc]==1)stop = i+1;//the place the block should stop
+                    else if(map[i+1][loc-1] == 1 || map[i+1][loc+1] == 1)stop = i+1;
+                }
+            }
+            else if(cmd[1] == '2'){
+
+            }
+            else if(cmd[1] == '3'){
+
+            }
+            else if(cmd[1] == '4'){
+
+            }
+        }
+    }
+
 
     
 }
